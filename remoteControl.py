6333123d09f13cbe7065f0dc287896a5d730fd48
kevinhuynh24 <59946896+kevinhuynh24@users.noreply.github.com>
2020-01-15 23:08:16 -0500

    Update remoteControl.py

--- remoteControl.py
+++ remoteControl.py
@@ -22,30 +22,35 @@ try:
             char = screen.getch()
             if char == ord('q'):
                 done = True
+            #Rotate both wheels forwards
             elif char == curses.KEY_UP:
                 print("up")
                 GPIO.output(7,False)
                 GPIO.output(11,True)
                 GPIO.output(8,False)
                 GPIO.output(10,True)
+            #Rotate both wheels backwards
             elif char == curses.KEY_DOWN:
                 print("down")
                 GPIO.output(7,True)
                 GPIO.output(11,False)
                 GPIO.output(8,True)
                 GPIO.output(10,False)
+            #Rotate left wheel forwards & Rotate right wheel backwards
             elif char == curses.KEY_RIGHT:
                 print("right")
                 GPIO.output(7,True)
                 GPIO.output(11,False)
                 GPIO.output(8,False)
                 GPIO.output(10,True)
+            #Rotate left wheel backwards & Rotate right wheel forwards
             elif char == curses.KEY_LEFT:
                 print("left")
                 GPIO.output(7,False)
                 GPIO.output(11,True)
                 GPIO.output(8,True)
                 GPIO.output(10,False)
+            #stop rotation
             elif char == 10:
                 print("end")
                 GPIO.output(7,False)
